ab73ace8dc07cb6ca5c42d4205161f04a41bd0c1
reisahaveri <reisa.haveri.001@student.uni.lu>
2024-03-19 09:44:04 +0100

    Employee created

---
+++ model/src/Employee.java
@@ -0,0 +1,63 @@
+public abstract class Employee {
+
+    private String name;
+    private String surname;
+    private int age;
+    private String role;
+    private String email;
+    private String id;
+    public abstract void printInfo();
+
+    public Employee(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
